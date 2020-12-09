47a7d479f350f7a9d17ed1c4f6eafa34119f828d
Mark Donaldson <markedon1980@gmail.com>
2020-12-09 09:08:06 +0000

    adds updated cleaning script

--- cleaning_scripts/cleaning_smoking.R
+++ cleaning_scripts/cleaning_smoking.R
@@ -7,26 +7,31 @@ smoking_scot_survey <- read_csv("data/raw_data/scot smoking survey data.csv")
 council_area <- read_csv("data/raw_data/council_area_codes.csv")
 
 # clean raw data
-smoking_scot_clean <- smoking_scot_survey %>%
+smoking_data_clean <- smoking_scot_survey %>%
   clean_names() %>% 
   mutate(date_code = as.ordered(date_code)) %>% 
   arrange(date_code) %>% 
-  filter(measurement == "Percent") %>% 
-  select(feature_code, date_code, units, value, gender)
+  filter(measurement == "Percent") %>%
+  filter(limiting_long_term_physical_or_mental_health_condition == "All") %>% 
+  filter(type_of_tenure == "All") %>% 
+  filter(household_type == "All") %>% 
+  separate(date_code, sep = "-",
+           into = c("date_start", "date_end"), remove = FALSE) %>%
+  mutate(date_diff = as.integer(date_end) - as.integer(date_start)) %>% 
+  filter(is.na(date_end)|(date_diff==1)) %>% 
+  filter(str_detect(feature_code, "S12"))
+
 
 # add council area data
 council_area_clean <- council_area %>%
   clean_names() %>% 
   select(feature_code = ca,
-         council_area = ca_name,
-         NHS_name = hb_name,
-         NHS_code = hb)
-council_area_clean
+         council_area = ca_name)
 
-# join the datasets (joining on feature code)
+# join the datasets (joining on feature code) & remove duplicate rows
 
-inner_join(smoking_scot_clean, council_area_clean)
+smoking_clean_join <- inner_join(smoking_data_clean, council_area_clean)
 
 # output clean data
-smoking_scot_clean %>% 
+smoking_clean_join %>% 
   write_csv("data/clean_data/clean_smoking.csv")
